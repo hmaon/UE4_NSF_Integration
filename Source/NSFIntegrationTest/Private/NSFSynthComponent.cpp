36e814ec5e1c96e0a0285974f37711d70fe08551
Greg V <hmaon@bumba.net>
2019-06-30 18:22:23 -0400

    gonna try to make NSFSynth stuff

---
+++ Source/NSFIntegrationTest/Private/NSFSynthComponent.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NSFSynthComponent.h"
+
+bool UNSFSynthComponent::Init(int32& SampleRate)
+{
+	NumChannels = 1;
+
+	// Initialize the DSP objects
+	Osc.Init(SampleRate);
+	Osc.SetFrequency(440.0f);
+	Osc.Start();
+
+	return true;
+}
+
+int32 UNSFSynthComponent::OnGenerateAudio(float* OutAudio, int32 NumSamples)
+{
+	// Perform DSP operations here
+	for (int32 Sample = 0; Sample < NumSamples; ++Sample)
+	{
+		OutAudio[Sample] = Osc.Generate();
+	}
+
+	return NumSamples;
+}
+
+void UNSFSynthComponent::SetFrequency(const float InFrequencyHz)
+{
+	// Use this protected base class method to push a lambda function which will safely execute in the audio render thread.
+	SynthCommand([this, InFrequencyHz]()
+	{
+		Osc.SetFrequency(InFrequencyHz);
+		Osc.Update();
+	});
+}
---
+++ Source/NSFIntegrationTest/Public/NSFSynthSound.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SynthComponent.h"
+#include "NSFSynthSound.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NSFINTEGRATIONTEST_API UNSFSynthSound : public USynthSound
+{
+	GENERATED_BODY()
+	
+};
